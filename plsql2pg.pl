54cc352a7fde2283881c7779a6244596d68e377a
Gilles Darold <gilles.darold@dalibo.com>
2017-01-31 21:47:23 +0100

    Extract sample queries into test/sql/sample_queries.sql file and allow sample file input from command line. An input DDL/DML file at command line is mandatory.

--- plsql2pg.pl
+++ plsql2pg.pl
@@ -544,37 +544,16 @@ o_pre_final_stars           ~ [*]*
 
 END_OF_DSL
 
-my $input = <<'SAMPLE_QUERIES';
-SElect 1 nb from DUAL WHERE rownum < 2; SELECT DISTINCT * from TBL t order by a nulls last, b desc, tbl.c asc;
-SELECT (1), nvl(val, 'null') || ' '|| nvl(val2, 'empty') "vAl",1, abc, "DEF" from "toto" as "TATA;";
- SELECT 1 + 2 * ((t.v) - 2) % 4 meh, 'test me', t.* from tbl t WHERE (((((a > 2)) and (rownum < 10)) OR ((((b < 3)))))) GROUP BY a, t.b;
- select * from (
-select 1 from dual
-) union (select 2 from dual) minus (select 3 from dual) interSECT (select 4 from dual) union all (select 5 from dual);
-select * from a,only (c) join b using (id,id2) left join d using (id) WHERE rownum >10 and rownum <= 20;
-select * from a,c right join b on a.id = b.id AND a.id2 = b.id2 naturaL join d CROSS JOIN e cj natural left outer join f natural full outer join g;
-select round(sum(count(*)), 2), 1 from a,b t1 where a.id = t1.id(+);
-SELECT id, log2(id), log10(id), count(*) FROM a GROUP BY id HAVING count(*)< 10;
-SELECT val, rank() over (partition by id) rank, lead(val) over (order by val rows CURRENT ROW), lag(val) over (partition by id,val order by val range between 2 preceding and unbounded following) as lag from t;
-WITH s1 as (with s3 as (select 1 from dual) select * from s3), s AS (SELECT * FROM s1 where rownum < 2) SELECT * From s, (with t as (select 3 from t) select * from t) cross join (with u as (select count(*) nb from dual) select nb from u union all (select 0 from dual)) where rownum < 2;
-with s as (select 1 from dual) SELECT employee_id, last_name, manager_id
-FROM employees   -- this is the FROM clause
-WHERE salary > 0   --should not happen
-start with /* hard coded value */ employee_id = 1 CONNECT BY isvalid = 1 and PRIOR employee_id = manager_id;
-SELECT a,b,c FROM foo bar group by grouping sets(a, cube(a,b), rollup(c,a), cube(rollup(a,b,c)));
-SELECT * FROM tbl t, t2 natural join t3 FOR UPDATE OF t2.a, col wait 1;
-update t set a = 1, (b,c) = (select * from t2 WHERE id = 1), d = (SELECT 1 from dual) where (a < 10);
-delete from public.t tbl where nvl(tbl.col, 'todel') = 'todel';
-insert into public.t ins values (2+1, 'tt');
-insert   into public.t ins (a,b) values (2+1, 'tt');
-insert into public.t ins (a,b) select id, count(*) from t group by 1;
--- now unsupported stuff
-SELECT 1 FROM t1 VERSIONS BETWEEN TIMESTAMP MINVALUE AND CURRENT_TIMESTAMP t WHERE id < 10;
-UPDATE t1 SET val = 't' WHERE id = 1 LOG ERRORS INTO err.log (to_char(SYSDATE), id);
-UPDATE t1 SET val = 't' WHERE id = 1 RETURNING (id%2), * INTO a,b REJECT LIMIT 3;
--- I don't belong to any query
-SAMPLE_QUERIES
-
+my $input = $ARGV[0] || '';
+if (!$input || !-f $ARGV[0]) {
+    die "FATAL: you must give a DDL/DML file to convert as argument.\n";
+}
+my $old_sep = $/;
+$/ = '';
+open(my $fh, $input) or die "FATAL: can not open file $input\n";
+$input = <$fh>;
+close($fh);
+$/ = $old_sep;
 
 print "Original:\n---------\n" . $input . "\n\nConverted:\n----------\n";
 
